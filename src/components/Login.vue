35216178a5932c9502e24e7b7dc7a765bda0022b
xiaofei <402715087@qq.com>
2020-03-31 21:20:22 +0800

    登录功能OJBK

---
+++ src/components/Login.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/dd16326b209129963e09e7bdaf62cbb.jpg" alt />
+      </div>
+      <!-- 登录表单 -->
+      <el-form
+        ref="loginFormRef"
+        class="login_form"
+        style="padding-left:20px; padding-right:20px"
+        :model="loginForm"
+        :rules="loginFormRules"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="el-icon-s-custom" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input type="password" prefix-icon="el-icon-unlock" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单验证规则
+      loginFormRules: {
+        // 验证用户名
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 12, message: '字符 3 到 12 个字符', trigger: 'blur' }
+        ],
+        // 验证密码
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '字符 6 到 12 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+
+  methods: {
+    // 重置表单
+    resetLoginForm() {
+      console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 登录
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return false
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登陆成功')
+        console.log(res)
+        // 登陆成功 token 保存到sessionStorage
+        window.sessionStorage.setItem('token', res.data.token)
+        // 登陆成功跳转到home页面
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+    .btns {
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 弹框组件挂载vue原型对象上
+Vue.prototype.$message = Message
